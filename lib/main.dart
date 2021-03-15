0506037b490946f69b1221dbddafa222590f45b2
Shashi7507 <snk7507@gmail.com>
2021-03-16 01:19:40 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,184 @@
+import 'dart:html' as html;
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shashi_portfolio/responsiveLayout.dart';
+import 'package:shashi_portfolio/styles.dart'; 
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  List<Widget> myLinks() => [
+        bottomButtonsWidget('Github', 'https://github.com/Shashi7507'),
+        SizedBox(
+          width: 10,
+        ),
+       
+        bottomButtonsWidget(
+            'Linkedin', 'https://www.linkedin.com/in/shashikant-khetmalis-66b25b95/'),
+        SizedBox(
+          width: 10,
+        ),
+        bottomButtonsWidget('Twitter', 'https://twitter.com/shashi_0'),
+        SizedBox(
+          width: 10,
+        ),
+        bottomButtonsWidget(
+            'Facebook', 'https://www.facebook.com/shashyaaaa/'),
+        SizedBox(
+          width: 10,
+        ),
+        bottomButtonsWidget(
+            'Instagram', 'https://www.instagram.com/shashyaa_1.0/'),
+      ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        padding: EdgeInsets.all(8.0),
+        child: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: <Widget>[
+              Expanded(
+                child: Center(
+                  child: ListView(
+                    shrinkWrap: true,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              SizedBox(
+                                height: ResponsiveLayout.isLargeScreen(context)
+                                    ? 200
+                                    : 100,
+                                width: ResponsiveLayout.isLargeScreen(context)
+                                    ? 200
+                                    : 100,
+                                child: CircleAvatar(
+                                  backgroundImage:
+                                      AssetImage('images/my_image.jpg'),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 40,
+                              ),
+                              Flexible(
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Hello',
+                                      style: titleTextStyle.copyWith(
+                                          fontSize:
+                                              ResponsiveLayout.isLargeScreen(
+                                                      context)
+                                                  ? 28
+                                                  : 22),
+                                    ),
+                                    Text(
+                                      'I\'m Shashikant Khetmalis',
+                                      style: titleNameStyle.copyWith(
+                                          fontSize:
+                                              ResponsiveLayout.isLargeScreen(
+                                                      context)
+                                                  ? 24
+                                                  : 18),
+                                    ),
+                                    Text(
+                                      'Computer Engineer and Full Stack Developer',
+                                      style: subHeadingTextStyle.copyWith(
+                                          fontSize:
+                                              ResponsiveLayout.isLargeScreen(
+                                                      context)
+                                                  ? 18
+                                                  : 12),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    FlatButton(
+                                        onPressed: () => {
+                                              html.window.open(
+                                                  'https://drive.google.com/open?id=1Ysf0zRhVKBCrYcyTXWjZ_VbZZ8bkAgoa',
+                                                  'Resume')
+                                            },
+                                        color: Colors.blue,
+                                        child: Text(
+                                          'Resume',
+                                          style: linksButtonStyle.copyWith(
+                                              color: Colors.white),
+                                        ))
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(
+                            height: 40,
+                          ),
+                          Wrap(
+                            children: myLinks(),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Made with ❤️ in Flutter',
+                        style: bottomFooterTextStyle,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+                      child: Text(
+                        'By Shashikant',
+                        style: bottomFooterTextStyle,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget bottomButtonsWidget(String text, String url) {
+  return InkWell(
+    onTap: () => {html.window.open(url, text)},
+    child: Text(
+      text,
+      style: linksButtonStyle,
+    ),
+  );
+}
---
+++ lib/styles.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+const titleTextStyle = TextStyle(
+    fontSize: 28.0,
+    color: Colors.black,
+    letterSpacing: 2.0,
+    fontFamily: 'Monda');
+
+const titleNameStyle = TextStyle(
+    fontSize: 24.0,
+    color: Colors.black,
+    letterSpacing: 2.0,
+    fontFamily: 'Righteous');
+
+const subHeadingTextStyle = TextStyle(
+    fontSize: 18.0,
+    color: Colors.black,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'Monda');
+
+const linksButtonStyle =
+    TextStyle(fontSize: 10.0, color: Colors.black, fontFamily: 'Monda');
+
+const bottomFooterTextStyle =
+    TextStyle(fontSize: 10.0, color: Colors.black, fontFamily: 'Monda');
